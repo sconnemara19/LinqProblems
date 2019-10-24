40d5e7ca718d2371d6232900136ec30dce7cbcce
Simone Connemara <simoneconnemara@outlook.com>
2019-10-24 07:44:19 -0500

    finished questions

--- LinqProblems/Question3.cs
+++ LinqProblems/Question3.cs
@@ -7,11 +7,20 @@ namespace LinqProblems
 {
     class Question3
     {
-        
 
-        
-        
-            
+
+        List<string> classGrades = new List<string>()
+            {
+                "80,100,92,89,65",
+                "93,81,78,84,69",
+                "73,88,83,99,64",
+                "98,100,66,74,55"
+
+
+
+            };
+
+
         //seperate the strings
         //change string into int
         //find out how to seperate the lowest number 
@@ -19,10 +28,10 @@ namespace LinqProblems
         //drop the lowest grade
         //average the grades from the rest of the string
         //average the averages 
-            
 
 
-        
+
+
         public double StudentAverage(List<string> grades)
         {
             var Temp = new List<double>();
